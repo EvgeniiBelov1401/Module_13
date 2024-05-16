f9488e7080e4de5226df04bd404ede1122a1b3da
EvgeniiBelov1401 <evgeniibelov1401@gmail.com>
2024-05-16 20:36:32 +0300

    Exercise-13-01-06

---
+++ Module-13/Exercise-13-01-06/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace Exercise_13_01_06
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string bookPath = @"D:\Programming\Skillfactory\C#_projects\Module_13\Module-13\Exercise-13-01-06\cdev_Text.txt";
+
+            string bookText=File.ReadAllText(bookPath);
+
+            char[] separators = {' ','\n','\r'};
+
+            var words=bookText.Split(separators,StringSplitOptions.RemoveEmptyEntries);
+
+            Console.WriteLine(words.Length);
+        }
+    }
+}
