ea86142b2305bcf19b01cac9f8eb7d6d0a6b06b7
EvgeniiBelov1401 <evgeniibelov1401@gmail.com>
2024-05-16 20:24:33 +0300

    Exercise-13-01-05

---
+++ Module-13/Exercise-13-01-05/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace Exercise_13_01_05
+{
+    internal class Program
+    {
+        static bool HowToSort(int[]arr)
+        {
+            bool sorted=true;
+            for (int i = 0; i < arr.Length-1; i++)
+            {
+                if (arr[i + 1] > arr[i])
+                {
+                    sorted = true;
+                }
+                else
+                {
+                    sorted = false;
+                    break;
+                }                               
+            }
+            if (sorted==true)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        static void Main(string[] args)
+        {
+            int[] array = { 1,2,3,4,5,6,7,8,9,10,56,6};
+
+            bool sort=HowToSort(array);
+            Console.WriteLine(sort);
+        }
+    }
+}
