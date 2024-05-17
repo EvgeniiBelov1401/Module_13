0c925277e241088518324b6e47f8c4db4ab81f75
EvgeniiBelov1401 <evgeniibelov1401@gmail.com>
2024-05-17 23:16:30 +0300

    Exercise-13-02-06

---
+++ Module-13/Exercise-13-02-06/Program.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Text;
+
+namespace Exercise_13_02_06
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            static ArrayList ListReMake(ArrayList list)
+            {
+                var arrayListRemake= new ArrayList();
+                var sum = 0;
+                StringBuilder text = new StringBuilder();
+
+                foreach (var item in list)
+                {
+                    if (item is int)
+                    {
+                        sum +=(int) item;
+                    }
+                    else
+                    {
+                        text.Append(item);
+                    }
+                }
+                arrayListRemake.Add(sum);
+                arrayListRemake.Add(text);
+
+                return arrayListRemake;
+            }
+
+            var arrayListStart = new ArrayList()
+            {
+                1,"Привет",12,"Как дела",100,"Hello",87,"world"
+            };
+
+            foreach(var item in ListReMake(arrayListStart))
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
