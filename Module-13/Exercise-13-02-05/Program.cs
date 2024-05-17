4e86f6b1e0f3b711ad2f6f4508ccc42ab19207b2
EvgeniiBelov1401 <evgeniibelov1401@gmail.com>
2024-05-17 22:48:41 +0300

    Exercise-13-02-05

---
+++ Module-13/Exercise-13-02-05/Program.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+
+namespace Exercise_13_02_05
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var months = new ArrayList()
+            {
+                "Jan","Feb","Mar","Apr","May","Jun","Jul","Aug","Sep","Oct","Nov","Des"
+            };
+
+            var numbers = new ArrayList()
+            {
+                1,2,3,4,5,6,7,8,9,10,11,12
+            };
+
+            var combinedList = new ArrayList();
+
+            foreach (var number in numbers)
+            {
+                combinedList.Add(months[(int)number-1]);
+                combinedList.Add(number);
+            }
+
+            foreach(var item in combinedList)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
