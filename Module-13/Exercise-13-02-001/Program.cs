e799fabec74af48139261d499f0ccca92dbd57f2
EvgeniiBelov1401 <evgeniibelov1401@gmail.com>
2024-05-17 21:14:11 +0300

    Exercise-13-02-001

---
+++ Module-13/Exercise-13-02-001/Program.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+
+namespace Exercise_13_02_001
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var list=new ArrayList() { 2,'d',"Строка"};
+
+            foreach(object o in list)
+            {
+                Console.WriteLine(o);
+            }
+        }
+    }
+}
