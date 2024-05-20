a1d160d78252ee0bbb8f08123733168500a87514
EvgeniiBelov1401 <evgeniibelov1401@gmail.com>
2024-05-20 16:03:15 +0300

    Exercise-13-03-003

---
+++ Module-13/Exercise-13-03-003/Program.cs
@@ -0,0 +1,49 @@
+﻿using System.Xml.Linq;
+
+namespace Exercise_13_03_003
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var names = new[]
+            {
+                "Анна",
+                "Алексей",
+                "Игорь",
+                "Юлия",
+                "Антон",
+                "Павел",
+                "Светлана",
+                "Антон",
+                "Юлия",
+                "Игорь"
+            };
+            Console.WriteLine($"Длина массива: {names.Length}\n");
+            foreach ( var name in names )
+            {
+                Console.WriteLine( name );
+            }
+            Console.WriteLine();
+            Console.ReadLine();
+
+
+            var uniqueNames =new HashSet<string>(names);
+            Console.WriteLine($"Длина хэш-сета: {uniqueNames.Count}\n");
+            foreach(var item in uniqueNames ) 
+            { 
+                Console.WriteLine( item ); 
+            }
+            Console.ReadLine();
+
+
+            uniqueNames.UnionWith(new[] { "Игорь", "Юлия","Андрей"});
+            Console.WriteLine($"Длина хэш-сета после объединения: {uniqueNames.Count}\n");
+            foreach (var item in uniqueNames)
+            {
+                Console.WriteLine(item);
+            }
+            Console.ReadLine();
+        }
+    }
+}
