c8274be587ee8019159c506f3a72f425fe164e8f
EvgeniiBelov1401 <evgeniibelov1401@gmail.com>
2024-05-20 13:44:35 +0300

    Exercise-13-03-001

---
+++ Module-13/Exercise-13-03-001/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace Exercise_13_03_001
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var contacts=new List<Contact>();
+
+            contacts.Add(new Contact("Ivan",79011234567,"ivan@mail.ru"));
+            contacts.Add(new Contact("Alex",89027654321,"alex@mail.ru"));
+
+            foreach (var contact in contacts)
+            {
+                Console.WriteLine(contact.Name+" "+contact.PhoneNumber+" "+contact.Email);
+            }
+        }
+    }
+}
