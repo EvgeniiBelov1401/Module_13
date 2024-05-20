6d1bac280e1fdb9bc55527fe98f1bc5206ceebb9
EvgeniiBelov1401 <evgeniibelov1401@gmail.com>
2024-05-20 16:47:35 +0300

    Console.WriteLine();

---
+++ Module-13/Exercise-13-03-004/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace Exercise_13_03_004
+{
+    internal class Program
+    {
+        static void PrintCollection(SortedSet<char> values)
+        {
+            foreach (char value in values)
+            {
+                Console.Write(value+" ");
+            }
+            Console.WriteLine();
+        }
+        static void Main(string[] args)
+        {
+            var collect1=new SortedSet<char>();
+            var collect2=new SortedSet<char>();
+
+            collect1.Add('B');
+            collect1.Add('A');
+            collect1.Add('D');
+            collect1.Add('C');
+            collect1.Add('Z');
+
+            collect2.Add('X');
+            collect2.Add('Y');
+            collect2.Add('Z');
+
+            PrintCollection(collect1);
+            PrintCollection(collect2);
+
+            Console.ReadLine();
+
+            collect1.ExceptWith(collect2);
+            PrintCollection(collect1);
+            PrintCollection(collect2);
+        }
+    }
+}
---
+++ Module-13/Exercise-13-03-10/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace Exercise_13_03_10
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = "Подсчитайте, сколько уникальных символов в этом предложении, используя HashSet<T>, учитывая знаки препинания, но не учитывая пробелы в начале и в конце предложения.";
+
+            Console.WriteLine(text);
+            Console.WriteLine();
+
+            var sentence = text.ToCharArray();
+
+            var uniqueChar=new HashSet<char>(sentence);
+            foreach(char ch in uniqueChar)
+            {
+                Console.Write(ch);
+            }
+            Console.WriteLine();
+            Console.WriteLine(uniqueChar.Count);
+
+        }   
+    }
+}
