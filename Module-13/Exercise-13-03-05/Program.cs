3a38d38ba7b4bb8e8adfcd83323e99bc8188a87a
EvgeniiBelov1401 <evgeniibelov1401@gmail.com>
2024-05-20 15:42:29 +0300

    Exercise-13-03-05

---
+++ Module-13/Exercise-13-03-05/Program.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+
+namespace Exercise_13_03_05
+{
+    internal class Program
+    {
+        static void FullYearList(List<string>month,ArrayList list)
+        {
+            var forAdding = new List<string>();
+
+            foreach (var item in list)
+            {
+                if (item is string)
+                {
+                    forAdding.Add((string)item);
+                }
+            }
+
+            month.AddRange(forAdding);
+
+            foreach (var item in month)
+            {
+                Console.WriteLine(item);
+            }
+        }
+        static void Main(string[] args)
+        {
+            var month = new List<string>
+            {
+                "Jan","Feb","Mar","Apr","May"
+            };
+
+            var missing = new ArrayList()
+            {
+               1, 2, 3, 5, "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"
+            };
+
+            FullYearList(month,missing);
+
+
+        }
+    }
+}
