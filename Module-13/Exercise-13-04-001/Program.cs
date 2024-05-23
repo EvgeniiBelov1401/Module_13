83f29a7b4ad0e7147be51b883b68342c4e747442
EvgeniiBelov1401 <evgeniibelov1401@gmail.com>
2024-05-23 13:49:12 +0300

    Exercise-13-04-001

---
+++ Module-13/Exercise-13-04-001/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace Exercise_13_04_001
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var cities = new Dictionary<string, string[]>();
+
+            cities.Add("Россия", new[] {"Москва","Санкт-Петербург","Казань"});
+            cities.Add("Украина", new[] { "Киев", "Львов", "Николаев", "Одесса" });
+            cities.Add("Беларусь", new[] { "Минск", "Витебск", "Гродно" });
+
+
+            //cities["Россия"] = new[] { "Мурманск", "Волгоград" };
+            //cities.Remove("Беларусь");
+            
+
+            foreach (var country in cities)
+            {
+                Console.Write(country.Key+": ");
+                foreach (var city in country.Value)
+                {
+                    Console.Write(city + " ");
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine();
+
+            
+
+            var russianCities = cities["Россия"];
+            Console.Write("Города России: ");
+            foreach (var city in russianCities)
+            {
+                Console.Write(city+" ");
+            }
+        }
+    }
+}
