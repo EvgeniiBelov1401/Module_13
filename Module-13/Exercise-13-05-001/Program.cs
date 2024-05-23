1f5d955c2ee143780a76a04a959a906699987b30
EvgeniiBelov1401 <evgeniibelov1401@gmail.com>
2024-05-23 15:42:29 +0300

    Exercise-13-05-001

---
+++ Module-13/Exercise-13-05-001/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Exercise_13_05_001
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var stack = new Stack<string>();
+            stack.Push("First");
+            stack.Push("Second");
+
+            //var element= stack.Pop();
+            var element=stack.Peek();
+            Console.WriteLine(element);
+        }
+    }
+}
