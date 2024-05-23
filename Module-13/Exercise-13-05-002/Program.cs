3138be98be0db573a54ef5a2fabc34306869d9a5
EvgeniiBelov1401 <evgeniibelov1401@gmail.com>
2024-05-23 16:22:35 +0300

    Exercise-13-05-04

---
+++ Module-13/Exercise-13-05-002/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace Exercise_13_05_002
+{
+    internal class Program
+    {
+        static void ShowStack(Stack<int> list)
+        {
+            foreach (var item in list)
+            {
+                Console.Write(item+" ");
+            }
+        }
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Random random = new Random();
+
+                var numbers = new Stack<int>();
+
+                for (int i = 0; i < 10; i++)
+                {
+                    numbers.Push(random.Next(100));
+                }
+
+                ShowStack(numbers);
+
+
+
+
+                Console.ReadLine();
+                Console.Clear();
+            }
+        }
+    }
+}
---
+++ Module-13/Exercise-13-05-04/Program.cs
@@ -0,0 +1,44 @@
+﻿namespace Exercise_13_05_04
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var words=new Stack<string>();
+
+            while (true)
+            {
+                Console.Write("Введите слово(слова): ");
+                var input = Console.ReadLine();
+                if (input == "pop")
+                {
+                    words.TryPop(out string popResult);
+                }
+                else if (input == "peek")
+                {
+                    words.TryPeek(out string peekResult);
+                    Console.Write(peekResult);
+                }
+                else
+                {
+                    words.Push(input);
+                }
+                
+
+                
+
+                Console.WriteLine();
+                Console.Write("Стэк: ");
+                if (words.Count == 0)
+                {
+                    Console.Write("-");
+                }
+                foreach (var word in words)
+                {
+                    Console.Write(word+" ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
